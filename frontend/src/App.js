ecf95e914fdc887da18d731502c6055526820417
Sergey Romanov <sergey.romanov@bostongene.com>
2022-03-19 20:05:47 +0300

    add frontend boilerplate
    - add antd as UI kit
    - add react-router-dom for navigation
    - add layout of the main page and basic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from "antd";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import "antd/dist/antd.css";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Layout>
+          <Header className="header">
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal">
+              <Menu.Item key="1">
+                <Link to="requests">Предложения</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="offers">Запросы</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: "0 50px" }}>
+            <Layout
+              className="site-layout-background"
+              style={{ padding: "24px 0" }}
+            >
+              <Content
+                style={{ padding: "0 24px", minHeight: "50vh", height: "" }}
+              >
+                <Routes>
+                  <Route path="/" element={<>mainPage</>} />
+                  <Route path="requests" element={<>requests</>} />
+                  <Route path="offers" element={<>offers</>} />
+                </Routes>
+              </Content>
+            </Layout>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>DecadenceIT ©2022</Footer>
+        </Layout>
+      </Router>
+    </>
   );
 }
 
